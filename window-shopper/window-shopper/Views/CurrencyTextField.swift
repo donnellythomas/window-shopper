a25adff49418d7eaaa408f77cc1d1163a31262d2
Thomas Donnelly <donnelly.thomas21@gmail.com>
2019-01-01 22:17:40 -0500

    added currency symbol

--- window-shopper/window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -9,6 +9,21 @@
 import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 10, y: (frame.size.height/2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7128499746, green: 0.7104290128, blue: 0.7147436142, alpha: 0.8)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -21,6 +36,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
     
     if placeholder == nil {
         placeholder = " "
